5efc9098c5b22bc701dd575e5646c917e5ba924f
StevBL <stevenbraloz007@gmail.com>
2020-08-26 11:46:09 -0600

    RestServer - usuario correccion

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=============================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://admin:JRdzjt97rSdmkQ8@cluster0.hcpvo.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:JRdzjt97rSdmkQ8@cluster0.hcpvo.mongodb.net/cafe';
+}
 
 process.env.URLDB = urlDB;
